ca10a1ab4c13391ec4c20794c7360b991916f1eb
Fernando <fernanguedezr@gmail.com>
2026-01-20 22:38:11 +0100

    update app.js

--- server.js
+++ server.js
@@ -1,24 +1,64 @@
 const express = require("express");
 const app = express();
+const { getProductsByCategory, productsData } = require("./filter");
 
 app.get("/", (req, res) => {
   res.send(`
     <h1>Bienvenido a la tienda</h1>
     <nav>
         <a href="/electronica">Electrónica</a> |
-        <a href="/">Ropa</a> |
-        <a href="/">Hogar</a> |
+        <a href="/ropa">Ropa</a> |
+        <a href="/hogar">Hogar</a> |
         <a href="/">Todos los productos</a>
     </nav>
     `);
 });
-app.get("/electronica", (req, res) => {
-    res.send(`<h1>Productos de electrónica</h1>`)
-})
 
+app.get("/electronica", (req, res) => {
+  const productos = getProductsByCategory("electrónica");
+  res.send(generateProductsPage("Sección de Electrónica", productos));
+});
+
+app.get("/ropa", (req, res) => {
+  const productos = getProductsByCategory("ropa");
+  res.send(generateProductsPage("Sección de Moda y Ropa", productos));
+});
+
+app.get("/hogar", (req, res) => {
+  const productos = getProductsByCategory("Hogar");
+  res.send(generateProductsPage("Todo para el Hogar", productos));
+});
+
+app.get("/productos", (req, res) => {
+  const html = generateProductsPage("Todos nuestros productos", productsData);
+  res.send(html);
+});
+
+function generateProductsPage(title, products) {
+  const productList = products
+    .map((p) => `
+    <li>
+      <strong>${p.name}</strong> - ${p.price}€ 
+      <br><small>Stock: ${p.stock} unidades</small>
+    </li>
+  `,
+    ).join(""); // Convertimos el array de strings en un solo string
+
+  return `
+    <h1>${title}</h1>
+    <p>Se han encontrado ${products.length} productos.</p>
+    <ul>
+      ${productList || "<li>No hay productos disponibles</li>"}
+    </ul>
+    <hr>
+    <a href="/">Volver a la tienda</a>
+  `;
+}
 
 app.use((req, res) => {
-  res.status(404).send("<h1>Page not found</h1><a href="/">Volver a la home</a>");
+  res
+    .status(404)
+    .send("<h1>Page not found</h1><a href='/'>Volver a la home</a>");
 });
 
 const PORT = process.env.PORT || 3000;
